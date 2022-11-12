595e688d8e681de8f51a9c5a8763d094f17097e5
simhani1 <simhani1@gmail.com>
2022-11-12 20:23:04 +0900

    docs: 파일 추가

---
+++ src/Client.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("192.168.0.41", 1234);
+            Thread thread = new Receiver(socket);
+            thread.start();
+        }
+        catch (Exception e) {
+            System.err.println("에러발생");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Unit.java
@@ -0,0 +1,12 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class Unit implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String food;
+    private int amount;
+}
