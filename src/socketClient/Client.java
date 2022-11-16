889a8a474ab81544198723f2f3180b128215b990
simhani1 <simhani1@gmail.com>
2022-11-16 18:15:31 +0900

    chore: IP 주소 삭제

--- src/socketClient/Client.java
+++ src/socketClient/Client.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("192.168.0.42", 1234);
+            Socket socket = new Socket("", 1234);
             Thread thread = new Receiver(socket);
             thread.start();
         }
