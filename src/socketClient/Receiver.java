b0c2981b6b7ce04a3cdf47df8dcfa1fcea7a2ad8
min9yu98 <kmkkms9698@naver.com>
2022-11-17 21:16:51 +0900

    fix: 잘못된 메시지 수신 처리 수정

--- src/socketClient/Receiver.java
+++ src/socketClient/Receiver.java
@@ -34,12 +34,10 @@ public class Receiver extends Thread implements Runnable {
             InputStream inputStream = socket.getInputStream();
             Protocol protocol = new Protocol();
             byte[] buf = protocol.getPacket();
-
             // 콘솔창에 입력받기 위한 스트림
             InputStream in = System.in;
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader br = new BufferedReader(reader);
-
             while (true) {
                 inputStream.read(buf);  // 서버에서 받은 바이트를 buf에 저장
                 int packetType = buf[0];
@@ -54,7 +52,6 @@ public class Receiver extends Thread implements Runnable {
                         System.out.println("비정상적인 유저입니다.");
                         socket.close();
                         break;
-
                     case Protocol.PT_LOGIN_RES:
                         id = protocol.getId();
                         System.out.println(id + " " + "환영합니다! 메뉴를 골라주세요");
@@ -62,7 +59,6 @@ public class Receiver extends Thread implements Runnable {
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
                     case Protocol.PT_LOGIN_REQ:
                         System.out.println("로그인을 해주세요");
                         System.out.print("ID를 입력하세요: ");
@@ -71,7 +67,6 @@ public class Receiver extends Thread implements Runnable {
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
                     case Protocol.PT_MAIN:
                         id = protocol.getId();
                         while (true){
@@ -80,9 +75,7 @@ public class Receiver extends Thread implements Runnable {
                             System.out.println("3. 포인트 충전");
                             System.out.println("4. 포인트 잔액 조회");
                             System.out.println("5. 서비스 종료");
-
                             main = br.readLine();
-
                             if (main.equals("1")) {
                                 protocol = new Protocol(Protocol.PT_STOCK_REQ);
                                 protocol.setId(id);
@@ -93,10 +86,8 @@ public class Receiver extends Thread implements Runnable {
                                     System.out.println("< 서비스 요청 사항 >");
                                     System.out.println("1. 휴지가 부족해요!");
                                     System.out.println("2. 물컵이 부족해요!");
-
                                     System.out.print("서비스 요청 사항 선택: ");
                                     request = br.readLine();
-
                                     if (Integer.parseInt(request) < 1 || Integer.parseInt(request) > 2) {
                                         System.out.println("잘못된 서비스 요청 사항입니다.");
                                         continue;
@@ -108,13 +99,11 @@ public class Receiver extends Thread implements Runnable {
                                 protocol.setServiceType(request);
                                 outputStream.write(protocol.getPacket());
                                 break;
-
                             } else if (main.equals("3")) { // 포인트 충전
                                 String inputPoint;
                                 while (true) {
                                     System.out.print("충전할 포인트을 입력해주세요: ");
                                     inputPoint = br.readLine();
-
                                     if (Integer.parseInt(inputPoint) > 100000) {
                                         System.out.println("최대 10만원까지 충전 가능합니다.");
                                         continue;
@@ -126,25 +115,18 @@ public class Receiver extends Thread implements Runnable {
                                 protocol.setClientPoint(inputPoint);
                                 outputStream.write(protocol.getPacket());
                                 break;
-
                             } else if (main.equals("4")) { // 잔여 포인트 조회
-//                                protocol = new Protocol(Protocol.PT_POINT_LOOKUP_REQ);
-//                                protocol.setId(id);
-//                                outputStream.write(protocol.getPacket());
+                                protocol = new Protocol(Protocol.PT_POINT_LOOKUP_REQ);
+                                protocol.setId(id);
+                                outputStream.write(protocol.getPacket());
                                 break;
-
                             } else if (main.equals("5")) {
-                                break;
+                                socket.close();
                             } else {
                                 System.out.println("잘못된 입력입니다.");
                             }
                             System.out.println();
                         }
-
-                        if (main.equals("4")){
-                            socket.close();
-                        }
-
                         System.out.println();
                         break;
 
@@ -155,11 +137,9 @@ public class Receiver extends Thread implements Runnable {
                         String[] menuList = fromString(protocol.getMenuName());
                         String[] amountList = fromString(protocol.getMenuAmount());
                         String[] priceList = fromString(protocol.getMenuPrice());
-
                         for (int i = 0; i < menuList.length; i++){
                             System.out.println((i + 1) + ". 메뉴:" + menuList[i] + " 남은 수량: " + amountList[i] + " 가격: " + priceList[i]);
                         }
-
                         // 메뉴 번호 입력 - 잘못입력시 while문 제대로 입력할 때까지
                         String menuName;
                         while (true){
@@ -172,7 +152,6 @@ public class Receiver extends Thread implements Runnable {
                             }
                             break;
                         }
-
                         // 메뉴 수량 입력 - 잘못입력시 while문 제대로 입력할 때까지
                         String menuAmount;
                         while (true){
@@ -185,42 +164,33 @@ public class Receiver extends Thread implements Runnable {
                             }
                             break;
                         }
-
                         protocol = new Protocol(Protocol.PT_ORDER);
                         protocol.setId(id);
                         protocol.setOrderFood(menuName);
                         protocol.setOrderAmount(menuAmount);
-
                         int total_order = Integer.parseInt(priceList[Integer.parseInt(menuName) - 1]) * Integer.parseInt(menuAmount);
                         System.out.println(total_order);
                         protocol.setOrderPrice(String.valueOf(total_order));
                         outputStream.write(protocol.getPacket());
                         break;
-
                     case Protocol.PT_POINT_RES:
                         System.out.println(protocol.getPointMsg());
-
                         protocol = new Protocol(Protocol.PT_MAIN); // 찾았다 시발련
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
-//                    case Protocol.PT_POINT_LOOKUP_RES:
-//                        System.out.println("현재 잔여 포인트: " + protocol.getClientPoint());
-//
-//                        protocol = new Protocol(Protocol.PT_MAIN);
-//                        protocol.setId(id);
-//                        outputStream.write(protocol.getPacket());
-//                        break;
-
-                    case Protocol.PT_SERVICE_RES:
-                        System.out.println(protocol.getServiceMsg());
-
+                    case Protocol.PT_LOOKUP_RES:
+                        System.out.println(protocol.getClientPoint());
                         protocol = new Protocol(Protocol.PT_MAIN);
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
+                    case Protocol.PT_SERVICE_RES:
+                        System.out.println(protocol.getServiceMsg());
+                        protocol = new Protocol(Protocol.PT_MAIN);
+                        protocol.setId(id);
+                        outputStream.write(protocol.getPacket());
+                        break;
                     case Protocol.PT_SHORTAGE_POINT:
                         // 잔액부족
                         System.out.println("잔액이 부족합니다.");
@@ -228,7 +198,6 @@ public class Receiver extends Thread implements Runnable {
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
                     case Protocol.PT_SHORTAGE_STOCK:
                         // 수량부족
                         System.out.println("입력 수량이 재고보다 많습니다.");
@@ -236,7 +205,6 @@ public class Receiver extends Thread implements Runnable {
                         protocol.setId(id);
                         outputStream.write(protocol.getPacket());
                         break;
-
                     case Protocol.PT_ORDER_SUCCESS:
                         System.out.println(id + " 님의 잔여 포인트: " + protocol.getClientPoint() + " 포인트");
                         // 성공
