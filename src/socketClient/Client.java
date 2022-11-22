474a1ee4fa2ca9c57b91d1624d80d06fd5841578
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-11-22 14:47:43 +0900

    Update Client.java

--- src/socketClient/Client.java
+++ src/socketClient/Client.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("192.168.35.105", 1234);
+            Socket socket = new Socket("ip주소", port);
             Thread thread = new Receiver(socket);
             thread.start();
         }
